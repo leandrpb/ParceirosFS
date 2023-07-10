1483ce3a52913a0b831fd288dac87a55b5052c26
Jesse-Calebe <62192464+Jesse-Calebe@users.noreply.github.com>
2023-07-10 19:36:44 -0300

    Implementação da rotina de Save no cenário de edição de dados

--- webapp/controller/Parceiro.controller.js
+++ webapp/controller/Parceiro.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast",
 ], function(
 	Controller,
-	JSONModel
+	JSONModel,
+	MessageToast
 ) {
 	"use strict";
 
@@ -17,6 +19,8 @@ sap.ui.define([
 			let oModel = new JSONModel();
 			oModel.setProperty("/habilitado", false);
 			this.getView().setModel(oModel, "editavel");
+			
+			this.getOwnerComponent().getModel().setDefaultBindingMode('TwoWay');
 		},
 		rotaDetalhe: function(oEvent) {
 			let sCodigoParceiro = oEvent.getParameters("arguments").arguments.CodigoParceiro;
@@ -30,12 +34,48 @@ sap.ui.define([
 			this.getView().bindElement(sCaminho);
 		},
 		onButtonEdit: function(oEvent) {
-			this._ConfiguraEdicao(true)
+			this._ConfiguraEdicao(true);
+		},
+		onButtonCancel: function(oEvent) {
+			this.getOwnerComponent().getModel().resetChanges();
+
+			this._ConfiguraEdicao(false);
+		},
+		onButtonSave: function(oEvent) {
+			let sCaminho = this.getView().getBindingContext().sPath;
+			let oDadosTela = this.getView().getBindingContext().getObject();
+			let oModel = this.getOwnerComponent().getModel();
+
+			let oInformacoesUpdate = {
+				CodigoParceiro: oDadosTela.CodigoParceiro,
+				Tipo: oDadosTela.Tipo,
+				Nome1: oDadosTela.Nome1,
+				Nome2: oDadosTela.Nome2,
+				TermoDePesquisa1: oDadosTela.TermoDePesquisa1,
+				TermoDePesquisa2: oDadosTela.TermoDePesquisa2,
+				Rua: oDadosTela.Rua,
+				NumeroCasa: oDadosTela.NumeroCasa,
+				Bairro: oDadosTela.Bairro,
+				Cidade: oDadosTela.Cidade,
+				Estado: oDadosTela.Estado,
+				Pais: oDadosTela.Pais,
+				CEP: oDadosTela.CEP
+			};
+
+			oModel.update(sCaminho, oInformacoesUpdate,{
+				success: () => {
+					MessageToast.show('Parceiro atualizado com sucesso!')
+					this._ConfiguraEdicao(false);
+				},
+				error: (oError) => {
+					MessageToast.show(JSON.parse(oError.responseText).error.message.value)
+				}
+			})
 		},
 		_ConfiguraEdicao: function(bHabilitaEdicao) {
 			let oModelEditavel = this.getView().getModel("editavel");
 
-			oModelEditavel.setProperty("/habilitado", bHabilitaEdicao)
+			oModelEditavel.setProperty("/habilitado", bHabilitaEdicao);
 		}
 	});
 });
