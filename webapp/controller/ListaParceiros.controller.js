416b68a6403c06631f682049a834e5fc1455834c
Jesse-Calebe <62192464+Jesse-Calebe@users.noreply.github.com>
2023-07-10 21:24:16 -0300

    Implementação rotina do botão Deletar Parceiro

--- webapp/controller/ListaParceiros.controller.js
+++ webapp/controller/ListaParceiros.controller.js
@@ -1,10 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/Dialog",
+    "sap/m/DialogType",
+    "sap/m/Text",
+    "sap/m/Button",
+    "sap/m/ButtonType"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast, Dialog, DialogType, Text, Button, ButtonType) {
         "use strict";
 
         return Controller.extend("zappfreestyle.parceiros.controller.ListaParceiros", {
@@ -34,6 +40,45 @@ sap.ui.define([
                 oRoteador.navTo("RouteParceiro", {
                     CodigoParceiro: "novo_parc"
                 })
+            },
+            onButtonDeletarParceiro: function (oEvent){
+                debugger;
+                this.sCaminho = this.getView().byId("idTabela").getSelectedItem().getBindingContext().sPath;
+            
+                if (!this.oApproveDialog) {
+                    this.oApproveDialog = new Dialog({
+                        type: DialogType.Message,
+                        title: "Deletar Parceiro",
+                        content: new Text({ text: "Tem certeza que deseja deletar o parceiro selecionado?" }),
+                        beginButton: new Button({
+                            type: ButtonType.Emphasized,
+                            text: "Sim",
+                            press: function () {
+                                let oModel = this.getOwnerComponent().getModel();
+
+                                oModel.remove(this.sCaminho, {
+                                    success: () => {
+                                        MessageToast.show('Parceiro deletado com sucesso!');
+                                    },
+                                    error: (oError) => {
+                                        MessageToast.show(JSON.parse(oError.responseText).error.message.value);
+                                    }
+                                })
+                                this.oApproveDialog.close();
+                            }.bind(this)
+                        }),
+                        endButton: new Button({
+                            text: "Não",
+                            press: function () {
+                                this.oApproveDialog.close();
+                            }.bind(this)
+                        })
+                    });
+                }
+    
+                this.oApproveDialog.open();
+
+                
             }
         });
     });
